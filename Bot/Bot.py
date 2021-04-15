e24006023f4ea9925f137e469918267fd52dc49c
KyklyMachine <71447642+KyklyMachine@users.noreply.github.com>
2021-04-15 16:02:35 +0300

    Реализовано сохранение оценок пользователя не поочередно, а в произвольном порядке.

--- Bot/Bot.py
+++ Bot/Bot.py
@@ -8,6 +8,7 @@ from psycopg2 import Error
 import threading
 import time
 
+
 #Импорт наших модулей
 import DataBase
 import RSS_Utils #Добавляем наши утилиты связанные с RSS
@@ -16,8 +17,7 @@ import Analize_Utils #Добавляем наши утилиты анализа 
 
 
 bot = telebot.TeleBot('1631242798:AAEBKc1x16vZpEO3QkzAecK5HEM8jE2v510')
-person_name = "No_Name"     #Имя гостя
-number_of_artical = 0       #Текущая статья
+person_name = "No_Name"     #Имя гостя (по умолчанию No_Name)
 page_count = 5              #Кол-во статей за 1 вывод
 page = 0                    #Текущая страница
 everydayNews = False        #Ежедневная рассылка новостей
@@ -35,21 +35,25 @@ def get_news():
         rss_list.append([f"   <b><u>{row[1]}</u></b>\n <b>Опубликовано:</b> {row[3]:%d/%m/%Y}\n <b>Сайт: {row[0]}</b>\n======================================\n{content[0:500]}", row[0]])
     return rss_list
 
+
 #Исправление ошибки с сертификатом ssl костыль просто отменяем проверку
 #context = ssl._create_unverified_context()
 ssl._create_default_https_context = ssl._create_unverified_context
 
+
 #Регистрация оценкок пользователя
-def register_grades(person_name, rss_list, grade, number_of_artical):
+def register_grades(person_name, rss_list, grade, number_of_artical, page):
+    global page_count
     DataBase.open_db_connection()
-    DataBase.write_one_row_in_censors(person_name, rss_list[number_of_artical][1], int(grade))
+    DataBase.write_one_row_in_censors(person_name, rss_list[int(number_of_artical)][1], int(grade))
+    print("Article number " + str(int(number_of_artical) + 1) + " written.")
     DataBase.close_db_connection()
+
 
 #Получение статей
 rss_list = get_news()
 
 
-#
 #TODO: допилить приветствие, сделать в нем регистрацию пользователя
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -93,34 +97,32 @@ def next_news(message):
     if (page + 1)*page_count < len(rss_list):
         for i in range(page_count*page, page_count*(page + 1)):
             keyboard = types.InlineKeyboardMarkup()
-            one_k = types.InlineKeyboardButton(text='1', callback_data='1')
-            two_k = types.InlineKeyboardButton(text='2', callback_data='2')
-            three_k = types.InlineKeyboardButton(text='3', callback_data='3')
-            four_k = types.InlineKeyboardButton(text='4', callback_data='4')
-            five_k = types.InlineKeyboardButton(text='5', callback_data='5')
+            one_k = types.InlineKeyboardButton(text='1', callback_data='1'+str(i))
+            two_k = types.InlineKeyboardButton(text='2', callback_data='2'+str(i))
+            three_k = types.InlineKeyboardButton(text='3', callback_data='3'+str(i))
+            four_k = types.InlineKeyboardButton(text='4', callback_data='4'+str(i))
+            five_k = types.InlineKeyboardButton(text='5', callback_data='5'+str(i))
             keyboard.add(one_k, two_k, three_k)
             keyboard.add(four_k, five_k)
             try:
                 bot.send_message(message.chat.id, rss_list[i][0], reply_markup=keyboard, parse_mode="HTML")
             except (Exception, Error) as error:
                 print("Ошибка при работе с PostgreSQL", error)
-                number_of_artical += 1
             time.sleep(1)
     else:
         for i in range(page_count*page, len(rss_list)):
             keyboard = types.InlineKeyboardMarkup()
-            one_k = types.InlineKeyboardButton(text='1', callback_data='1')
-            two_k = types.InlineKeyboardButton(text='2', callback_data='2')
-            three_k = types.InlineKeyboardButton(text='3', callback_data='3')
-            four_k = types.InlineKeyboardButton(text='4', callback_data='4')
-            five_k = types.InlineKeyboardButton(text='5', callback_data='5')
+            one_k = types.InlineKeyboardButton(text='1', callback_data='1'+str(i))
+            two_k = types.InlineKeyboardButton(text='2', callback_data='2'+str(i))
+            three_k = types.InlineKeyboardButton(text='3', callback_data='3'+str(i))
+            four_k = types.InlineKeyboardButton(text='4', callback_data='4'+str(i))
+            five_k = types.InlineKeyboardButton(text='5', callback_data='5'+str(i))
             keyboard.add(one_k, two_k, three_k)
             keyboard.add(four_k, five_k)
             try:
                 bot.send_message(message.chat.id, rss_list[i][0], reply_markup=keyboard, parse_mode="HTML")
             except (Exception, Error) as error:
                 print("Ошибка при работе с PostgreSQL", error)
-                number_of_artical += 1
     page += 1
 
 
@@ -135,17 +137,19 @@ def everydayNews_YN(message):
 
 
 #Получаем ответ пользователя, т.е. обработка всех ответов кнопочек и всего подобного
-#TODO: реализовать сохранение оценки не поочередно, а с помощью ID ответа
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
     global rss_list
-    global number_of_artical
-    if call.data == "1" or call.data == "2" or call.data == "3" or call.data == "4" or call.data == "5":
-        register_grades(person_name, rss_list, call.data, number_of_artical)
+    global page;
+    if call.data[0] in ("1", "2", "3", "4", "5"):
+        register_grades(person_name, rss_list, call.data[0], call.data[1], page)
         bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
-        number_of_artical += 1
-        bot.send_message(call.message.chat.id, "Ваши оценка статьи записана!")
+        bot.send_message(call.message.chat.id, "Ваша оценка статьи записана!")
     global everydayNews
+
+
+
+
     if call.data == "yes_everydayNews":
         everydayNews = True
         bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
