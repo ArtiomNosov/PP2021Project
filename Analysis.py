1cc8f01827883c7aae45f0a06df8ede5252797cd
KyklyMachine <71447642+KyklyMachine@users.noreply.github.com>
2021-05-16 20:12:09 +0300

    Убраны закомментированные строки

--- Analysis.py
+++ Analysis.py
@@ -15,13 +15,13 @@ analysisFrequency = 20  # Раз в день 8640000000
 
 # Функция запускающая таймер
 def analysis_grades():
-    #DataBase.open_db_connection()
+    DataBase.open_db_connection()
 
     sql_request_str = 'SELECT id_censors, person_name, email, everyday_news FROM censors'
 
     DataBase.cursor.execute(sql_request_str)
     result = DataBase.cursor.fetchall()
-    #DataBase.close_db_connection()
+    DataBase.close_db_connection()
     print(result)
     for iterator in result:
         print(iterator[0])
